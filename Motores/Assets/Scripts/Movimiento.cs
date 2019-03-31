ca894ba7ddbf6556a520a62c5fc11d4a8b43e541
JDex18 <44837818+JDex18@users.noreply.github.com>
2019-04-01 01:44:31 +0200

    Movimiento de personaje y cámara

---
+++ Motores/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+
+    public float velocidad = 6f;
+
+    Vector3 movimiento;//contiene la x, y, z del personaje
+    Rigidbody playerRigidbody;
+
+
+    void Awake()
+    {
+
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");//input para la tecla de movimiento en x
+        float v = Input.GetAxisRaw("Vertical");//input para la tecla de movimiento en z
+
+        Move(h, v);
+
+      
+    }
+
+    void Move(float h, float v)
+    {
+        movimiento.Set(h, 0f, v);//asigna las teclas pulsadas al vector posición del personaje
+        movimiento = movimiento.normalized * velocidad * Time.deltaTime;//normaliza el vector para que al ir en diagonal no se tenga ventaja
+        playerRigidbody.MovePosition(transform.position + movimiento);//actualiza la posición del personaje según lo que se ha tecleado
+    }
+}
