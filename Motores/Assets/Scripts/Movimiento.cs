0d7096d76ac0703b13bb8cba78b40e23a8aa9e68
Jon Hodei Martínez <al361886@uji.es>
2019-04-01 14:13:47 +0200

    Personaje añadido
    Esta el personaje con el movimiento y lo de que al mover el cursor gire (como en el del Zombunny). La camara esta en perspectiva y el suelo tiene textura

--- Motores/Assets/Scripts/Movimiento.cs
+++ Motores/Assets/Scripts/Movimiento.cs
@@ -9,11 +9,15 @@ public class Movimiento : MonoBehaviour {
 
     Vector3 movimiento;//contiene la x, y, z del personaje
     Rigidbody playerRigidbody;
+    Animator anim;
+    int floorMask;
+    float camRayLength = 100f;
 
 
     void Awake()
     {
-
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody>();
 
     }
@@ -25,14 +29,43 @@ public class Movimiento : MonoBehaviour {
         float v = Input.GetAxisRaw("Vertical");//input para la tecla de movimiento en z
 
         Move(h, v);
+        Turning();
+        Animating(h, v);
 
-      
     }
 
     void Move(float h, float v)
     {
+
         movimiento.Set(h, 0f, v);//asigna las teclas pulsadas al vector posición del personaje
         movimiento = movimiento.normalized * velocidad * Time.deltaTime;//normaliza el vector para que al ir en diagonal no se tenga ventaja
         playerRigidbody.MovePosition(transform.position + movimiento);//actualiza la posición del personaje según lo que se ha tecleado
+
+    }
+
+    void Turning()
+    {
+
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+
+    }
+
+
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("isWalking", walking);
     }
 }
