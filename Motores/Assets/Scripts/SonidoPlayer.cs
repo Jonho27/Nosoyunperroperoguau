0ede4e1fbb9d0f531419b9d11ba4119988ee0ad4
pauborrell <pauborrellegea@gmail.com>
2019-04-08 20:17:05 +0200

    Sonido Andar (no completo)

---
+++ Motores/Assets/Scripts/SonidoPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoPlayer : MonoBehaviour {
+
+	public AudioClip walk;
+
+	AudioSource fuenteAudio;
+
+	// Use this for initialization
+	void Start () {
+		fuenteAudio = GetComponent<AudioSource> ();
+		fuenteAudio.clip = walk;
+		fuenteAudio.loop = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)){
+			fuenteAudio.Play();
+			
+		}
+
+		else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+		{
+			fuenteAudio.Stop();
+			fuenteAudio.loop = false;
+
+		}
+		
+	}
+}
