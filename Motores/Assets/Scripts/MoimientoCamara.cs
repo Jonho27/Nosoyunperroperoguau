5734623a48f0f04ac46d15a0d1cb93a8706b6e9a
Jon Hodei Martínez <al361886@uji.es>
2019-04-09 15:53:42 +0200

    Pues no

---
+++ Motores/Assets/Scripts/MoimientoCamara.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoimientoCamara : MonoBehaviour {
+
+    public Transform objetivo;
+    public float suavidad = 5f;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - objetivo.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = objetivo.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, suavidad + Time.deltaTime);
+    }
+}
