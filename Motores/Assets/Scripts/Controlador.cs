3830307271152f23c1a3e7e2c7a4a5610dcb4d93
pauborrell <pauborrellegea@gmail.com>
2019-04-19 22:10:44 +0200

    Llave Para salir

---
+++ Motores/Assets/Scripts/Controlador.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    public int cantidadRecogida = 0;
+
+    public void RecogerObjeto(int cantidad){
+
+        cantidadRecogida += cantidad;
+
+
+    }
+}
---
+++ Motores/Assets/Scripts/Recoger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoger : MonoBehaviour
+{
+   public int cantidad = 1;
+   public bool key = false;
+
+   void Update()
+    {
+        
+        if (Input.GetKeyDown("space") && key == true)
+        {
+            //print("space key was pressed");
+            Destroy(gameObject);
+        }
+    }
+
+   void OnTriggerEnter(Collider Personaje){
+       if (Personaje.CompareTag("FPS")){
+           Personaje.GetComponent<Controlador>().RecogerObjeto(cantidad);
+           key = true;
+       }
+   }
+}
