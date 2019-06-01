0b4d0c6bc95a1c2f934de129c7a2403ebe0ed807
JDex18 <44837818+JDex18@users.noreply.github.com>
2019-06-01 17:10:36 +0200

    HUD

---
+++ Motores/Assets/Scripts/LlavesManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LlavesManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameControlManager.abrirPuerta == true)
+        {
+            gameObject.GetComponent<Image>().color = new Color32(255, 255, 225, 255);
+        }
+    }
+}
