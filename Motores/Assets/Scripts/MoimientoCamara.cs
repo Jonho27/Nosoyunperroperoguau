1f77abf3ccd8032df1859e9ab0ff144c3783e20e
JDex18 <44837818+JDex18@users.noreply.github.com>
2019-06-03 15:12:33 +0200

    Camara

--- Motores/Assets/Scripts/MoimientoCamara.cs
+++ Motores/Assets/Scripts/MoimientoCamara.cs
@@ -11,11 +11,19 @@ public class MoimientoCamara : MonoBehaviour {
 
     Vector3 offset;
     Vector3 targetCamPos;
+    GameObject camaraZoom;
+
+    private float initHeightAtDist;
+    private bool dzEnabled;
+
+    Camera camara;
 
     void Awake()
     {
         offset = transform.position - objetivo.position;
         zoom = false;
+        //camaraZoom.transform.localPosition = new Vector3(0, 5.6f, 0);
+        camara = GetComponent<Camera>();
     }
 
     void FixedUpdate()
@@ -27,8 +35,10 @@ public class MoimientoCamara : MonoBehaviour {
             objetivo = GameObject.FindGameObjectWithTag("Enemigo").transform;
             targetCamPos = objetivo.position + offset;
             transform.position = Vector3.Lerp(transform.position, targetCamPos, suavidad + Time.deltaTime);
-            
+            camara.orthographicSize = 7.0f;
+
         }
     }
+
 
 }
