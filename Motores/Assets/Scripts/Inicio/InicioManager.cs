2891984861980b76eb9cf7d8459e51a4a2f3eb5b
JDex18 <44837818+JDex18@users.noreply.github.com>
2019-06-04 12:39:29 +0200

    Pantalla de carga y HUD

---
+++ Motores/Assets/Scripts/Inicio/InicioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InicioManager : MonoBehaviour
+{
+
+    public Image image;
+
+    float speed = 1f;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= 1)
+        {
+            image.color = Color.Lerp(image.color, Color.clear, speed * Time.deltaTime);
+        }
+
+        if(timer >= 4)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        //image.color = Color.Lerp(image.color, Color.clear, speed * Time.deltaTime);
+
+    }
+}
---
+++ Motores/Assets/Scripts/Managers/HudManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour
+{
+    public Image monedero;
+    public Image mochila;
+    public Image llave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameControlManager.cartera == true)
+        {
+            monedero.color = new Color32(255, 255, 225, 255);
+        }
+
+        if (GameControlManager.mochila == true)
+        {
+            mochila.color = new Color32(255, 255, 225, 255);
+        }
+
+        if (GameControlManager.abrirPuerta == true)
+        {
+            llave.color = new Color32(255, 255, 225, 255);
+        }
+
+    }
+}
